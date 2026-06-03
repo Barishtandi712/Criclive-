e5be7efaf4047d4c3628d493f9990178beca2b6f
Barishtandi712 <barishtandi712@gmail.com>
2026-06-04 01:51:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,21 +20,20 @@ def live_matches():
             "User-Agent": "Mozilla/5.0"
         }
 
-response = requests.get(link, headers=headers, timeout=20)
-response.raise_for_status()
+        response = requests.get(link, headers=headers, timeout=20)
+        response.raise_for_status()
 
-page = BeautifulSoup(response.text, "html.parser")
+        page = BeautifulSoup(response.text, "html.parser")
 
         matches = []
 
-for match in page.find_all("div", class_="cb-scr-wll-chvrn"):
-    matches.append(match.get_text(" ", strip=True))
+        for match in page.find_all("div", class_="cb-scr-wll-chvrn"):
+            matches.append(match.get_text(" ", strip=True))
 
-return jsonify({
-    "status": "success",
-    "matches": matches
- }
-        
+        return jsonify({
+            "status": "success",
+            "matches": matches
+        })
 
     except Exception as e:
         return jsonify({
