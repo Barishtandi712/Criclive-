e218e044fac618b0f19d0ac15b37457591043d5c
Barishtandi712 <barishtandi712@gmail.com>
2026-06-04 04:13:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,3 +44,10 @@ def live_matches():
         return jsonify({
             "error": str(e)
         }), 500
+import os
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
